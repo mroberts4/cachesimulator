fddc239bc62e66307c55780414fd84ed95e4149c
David Harmon <74120288+koronusTraveler@users.noreply.github.com>
2021-10-09 12:37:33 -0500

    Update fifo.h

--- fifo.h
+++ fifo.h
@@ -1,5 +1,16 @@
+#include <cstdint>
+#include <math.h>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <bitset>
+#include <sstream>
+#include <stdio.h>
+#include <unordered_map>
+#include <list>
+#include <algorithm>
 
-
+//this is the fifo class file to perform first in first out operations on the cache
 
 
 
