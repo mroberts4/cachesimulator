3c6661edf6a809445de0b2ddcb1b8e8b903c74f6
David Harmon <74120288+koronusTraveler@users.noreply.github.com>
2021-10-09 11:01:13 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <cstdint>
+#include <math.h>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <bitset>
+#include <sstream>
+#include <stdio.h>
+#include <unordered_map>
+#include <list>
+#include <algorithm>
+
+//Main file for the architecture project
+
+
+
+
+
+
+
+
+
+public class void fifo()
+{
+
+	for (int i = 0; i < setSize; i++)
+	{
+
+		if (i > stackSize)
+		{
+			stack.pop_front();
+		}
+		stack.push_back(x);
+	}
+}
