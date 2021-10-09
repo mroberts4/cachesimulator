544ede0f67d046d1bbeaa4f0f2a789983034c294
mroberts4 <56057346+mroberts4@users.noreply.github.com>
2021-10-09 12:45:34 -0500

    Updates
    Added public and public variables to the LRU class along with constructor and destructors

--- LRU_Class.h
+++ LRU_Class.h
@@ -8,22 +8,44 @@
 #include <sstream>
 #include <stdio.h>
 #include <fstream>
+#include <unordered_map> 
+#include <cstdint>
+#include <math.h>
 
 using namespace std;
 
 namespace 6
 {
 	class LRU{
-		typedef bitset<32>bits;
+		typedef bitset<32> bits;
+		
+		/*structure for the memory address for the cache */
+		struct memory_address
+		{
+			bits mem;
+			bits tag; 
+			bits index;
+			bits offset;
+		};
 		
 		private: 
+			/*Calculated values*/
 			int blocks; 
 			int sets; 
 			int index;
+			int hits;
+			int misses;
+			int offset; 
+			int evicts; 
+			
+		/*LRU functions to be implemented based on the cofiurations from the text file.*/
 		
-		
-		
-		
+		public:
+			/*Construtor and destructor*/
+                        LRU (int&);
+			~LRU();
+			void readMemory();
+			void TestMemory();
 	};
 }
 
