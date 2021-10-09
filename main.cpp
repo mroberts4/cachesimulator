7e0d84fd78c7d9f6271defaa2d052ef65ddb99c8
David Harmon <74120288+koronusTraveler@users.noreply.github.com>
2021-10-09 13:10:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,8 +15,20 @@
 
 
 
+int main(int argc, char** argv)
+{
+	int c,b,n;
+	readConfig(c,b,n);	//read in configuration values from file
 
-
-
+	/*Instantiate cache class with config file params*/
+	Cache cache1(c,b,n);
+  Cache cache2(c,b,n);
+  cache1.readMemory();
+	cache1.testMemLRU();
+	cache2.readMemory();
+	cache2.testMemFifo();
+	
+	return 0;
+}
 
 
