a07823ddf87f46c4f2512e3f6c150d6e86ff42c5
mroberts4 <56057346+mroberts4@users.noreply.github.com>
2021-10-09 11:20:59 -0500

    Add files via upload
    The LRU class file (draft) source code

---
+++ LRU_Class.h
@@ -0,0 +1,30 @@
+#ifndef LRU_CLASS_H
+#define LRU_CLASS_H
+
+#include <list>
+#include <algorithm> //for finding in a list
+#include <array>
+#include<iostream>
+#include <sstream>
+#include <stdio.h>
+#include <fstream>
+
+using namespace std;
+
+namespace 6
+{
+	class LRU{
+		typedef bitset<32>bits;
+		
+		private: 
+			int blocks; 
+			int sets; 
+			int index;
+		
+		
+		
+		
+	};
+}
+
+#endif
